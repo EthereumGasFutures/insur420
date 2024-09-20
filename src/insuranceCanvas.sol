686a27c979dce415fbbc3c5b3e56e8e72a91b9aa
Miller Mendoza Jimenez <millmen@serpius.ch>
2024-09-20 18:31:27 +0800

    adding some files

---
+++ src/insuranceCanvas.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+contract InsuranceCanvas {
+
+    constructor() {}
+
+    function someData() public pure returns( address ) {
+        return address(0);
+    }
+
+}
